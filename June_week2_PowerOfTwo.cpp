7536858aa3fa852a25a832cc0a16bccb29cfb1ea
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2020-07-20 18:15:03 +0530

    Update June_week2_PowerOfTwo.cpp

--- June_week2_PowerOfTwo.cpp
+++ June_week2_PowerOfTwo.cpp
@@ -7,3 +7,17 @@ public:
         return false;
     }
 };
+
+class Solution2 {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+       while(n>1){
+           if(n%2!=0){
+               return false;
+           }
+           n=n>>1;
+       }
+        return true;
+    }
+};
